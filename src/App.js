793d7c775eeefb11bc30a65dd556afd7720906b9
zygis <zygis.1415@outlook.es>
2020-04-20 00:26:21 +0200

    Initial project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './assets/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>BUENOS DIAS PRINCESO</h2>
+    </div>
+  );
+}
+
+export default App;
